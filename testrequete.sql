d14a5a4e439b9cfa38d95e4b16bf9c3bab89432b
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-03-18 13:43:28 +0100

    maj avec explain

--- testrequete.sql
+++ testrequete.sql
@@ -1,7 +1,15 @@
 /*Création/Modification/Suppression/Interet des index
 Sauvegarde / Restauration
 Les journaux (logs) de requêtes, erreurs, etc.
+
 Optimisation des requêtes(EXPLAIN)
+L'optimisation de requête est une opération dans laquelle plusieurs plans d'exécution d'une requête SQL 
+sont examinés pour en sélectionner le meilleur. L'estimation de leurs coûts dépend du temps d'exécution 
+et du nombre de ressources utilisées pour y parvenir, elle se mesure en entrées-sorties.
+
+Cela se fait grâce à EXPLAIN : Analyse les requêtes en affichant le plan d’exécution d’une requête SQL. 
+Cela permet de savoir de quelle manière le Système de Gestion de Base de Données (SGBD) 
+va exécuter la requête et s’il va utiliser des index et lesquels.
 
 SQL :
  INTERSECT, EXCEPT

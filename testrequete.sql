9ee245df6a7017dbe0c28b16a4c0109741c121a9
Althalas <martin.petitcontactpro@gmail.com>
2025-03-18 15:11:57 +0100

    aled

--- testrequete.sql
+++ testrequete.sql
@@ -36,9 +36,15 @@ La commande SQL INTERSECT permet d’obtenir l’intersection des résultats de 
 pour l’utiliser convenablement il faut que les 2 requêtes retourne le même nombre de colonnes, avec les mêmes types et dans le même ordre.
 */
 
-CREATE TABLE utilisateur1;
+CREATE TABLE utilisateur1 (id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    age INT,);
 
-CREATE TABLE utilisateur2;
+CREATE TABLE utilisateur2 (id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    age INT,);
 
 INSERT INTO utilisateur1 (id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(100),
@@ -65,21 +71,27 @@ A savoir : cette commande s’appelle différemment selon les Systèmes de Gesti
 EXCEPT : PostgreSQL
 MINUS : MySQL et Oracle
 */
-CREATE TABLE user_aigris;
-
-CREATE TABLE user_pas_aigris;
-
-INSERT INTO user_aigris (id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE user_aigris (id_aigris INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(100),
     prenom VARCHAR(100),
-    age INT,) 
+    age INT);
+
+CREATE TABLE user_pas_aigris (id_pas_aigris INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    age INT);
+
+INSERT INTO user_aigris (id_aigris,
+    nom,
+    prenom,
+    age) 
     VALUES (1, 'Aled', 'Alice', 29),(2, 'Lapin', 'Garen', 60),(3, 'Petit', 'Martin', 26),(4, 'Guillet', 'Vincent', 26),(5, 'Gay', 'Virginie', 34),(6, 'Oskour', 'Jeanne', 96);
 
 
-INSERT INTO user_pas_aigris(id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100),
-    prenom VARCHAR(100),
-    age INT,) 
+INSERT INTO user_pas_aigris(id_pas_aigris,
+    nom,
+    prenom,
+    age)
     VALUES (2, 'Lapin', 'Garen', 60),(4, 'Guillet', 'Vincent', 26),(5, 'Gay', 'Virginie', 34);
 
 SELECT * FROM user_aigris

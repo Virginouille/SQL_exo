15f952d42a65373153caac53b572d00221bb58c4
Althalas <martin.petitcontactpro@gmail.com>
2025-03-18 12:33:29 +0100

    Create testrequete.sql

---
+++ testrequete.sql
@@ -0,0 +1,12 @@
+/*Création/Modification/Suppression/Interet des index
+Sauvegarde / Restauration
+Les journaux (logs) de requêtes, erreurs, etc.
+Optimisation des requêtes(EXPLAIN)
+
+SQL :
+ INTERSECT, EXCEPT
+ ANY/SOME, ALL 
+ APPLY (CROSS, OUTER) 
+ OVER, RANK, ROW_NUMBER
+ CASE 
+ TRANSACTION*/
